2af030e4044863bb0b1751bac55b29e11ab101fa
haoxle <hao-le@live.co.uk>
2023-02-19 13:01:21 +0000

    RobotCoordinates

--- src/main/java/org/example/RobotString.java
+++ src/main/java/org/example/RobotString.java
@@ -5,19 +5,18 @@ import java.util.Arrays;
 public class RobotString {
 
     public int[] getCoordinates(String instruction) {
-        instruction.toUpperCase().replace("UP", "U").replace("DOWN", "D").replace("LEFT", "L").replace("RIGHT", "R");
         int x = 0;
         int y = 0;
         for (int i = 0; i < instruction.length() ; i++) {
             if(instruction.charAt(i) == 'U') {
-                x++;
+                y++;
             }
             if(instruction.charAt(i) == 'D') {
-                x--;
-            } if(instruction.charAt(i) == 'L') {
                 y--;
+            } if(instruction.charAt(i) == 'L') {
+                x--;
             } if(instruction.charAt(i) == 'R') {
-                y++;
+                x++;
             }
         }
         int[] arr = {x,y};
--- src/test/java/org/example/RobotStringTest.java
+++ src/test/java/org/example/RobotStringTest.java
@@ -11,12 +11,12 @@ class RobotStringTest {
     @Test
     void getCoordinatesofUUU() {
         RobotString rs = new RobotString();
-        assertEquals("[3, 0]", Arrays.toString(rs.getCoordinates("UUU")));
+        assertEquals("[0, 3]", Arrays.toString(rs.getCoordinates("UUU")));
     }
     @Test
     void getCoordinatesofWords() {
         RobotString rs = new RobotString();
-        assertEquals("[-1, 0]", Arrays.toString(rs.getCoordinates("Up Down Left 2xRight Down")));
+        assertEquals("[0, -1]", Arrays.toString(rs.getCoordinates("Up Down Left 2xRight Down")));
     }
     @Test
     void getCoordinatesofURDL() {
